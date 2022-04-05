70afae6ceb1f934ea499ba7cd7475798dcdf8f04
German Torales <germantorales73@gmail.com>
2022-04-05 19:10:51 -0300

    Add media queries

--- src/pages/Photos/PhotoCard.jsx
+++ src/pages/Photos/PhotoCard.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './Photos.styles.css';
 
 const PhotoCard = ({ photo }) => {
+  const { innerWidth: width } = window;
+  const title = width < 768 ? `#${photo.id}` : `#${photo.id} ${photo.title.substring(0, 20)}`;
+
   return (
     <div className="photo__container">
       <div className="photo__card">
         <div className="card__title">
-          <p>
-            #{photo.id} {''}
-            {photo.title.substring(0, 18)}...
-          </p>
+          <p>{title}</p>
         </div>
         <div className="card__photo">
           <img src={photo.url} alt={photo.albumId} />
