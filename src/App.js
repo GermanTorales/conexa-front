c486ac27364755bd0a4594e59ac1f43f368f4b5b
German Torales <germantorales73@gmail.com>
2022-04-04 22:43:38 -0300

    Add home route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home, Login } from "./pages";
 
 import "./global.styles.css";
-import { Login } from "./pages";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route exact path="/login" element={<Login />} />
+        <Route exact path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
